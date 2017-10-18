047283b4083c296c87f9aa26d0d4a1f463f28904
Garrett <nova4005@gmail.com>
2017-10-18 18:46:21 -0400

    added some ideas

--- resources/views/languages/jquery.blade.php
+++ resources/views/languages/jquery.blade.php
@@ -6,7 +6,21 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">jQuery</div>
-
+                <div class="panel-body">
+                    <h4>Just jotting down ideas for now</h4>
+                    <ul>
+                        <li>build a mortgage calculator</li>
+                        <li>build a slide out menu</li>
+                        <li>Build ajax pagination for WP blog roll</li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/languages/php.blade.php
+++ resources/views/languages/php.blade.php
@@ -6,7 +6,21 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">PHP</div>
-
+                <div class="panel-body">
+                    <h4>Just jotting down ideas for now</h4>
+                    <ul>
+                        <li>build a WordPress plugin that has a custom settings page with various theme adjustments.</li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
